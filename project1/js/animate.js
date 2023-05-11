d2d8c6e21eeb7da9053299fac0ca7cc493c799a0
joshii04 <joshua.sinclair04@gmail.com>
2023-05-11 17:01:23 +0100

    finished proj1
    simple js

---
+++ project1/js/animate.js
@@ -0,0 +1,8 @@
+// learning js from the beggining again
+
+
+
+document.getElementById('but').addEventListener('click', function(){
+    document.getElementById('change-me').innerHTML = 'I love maths!'; 
+    //inner html allows you to write straight into HTML
+})
