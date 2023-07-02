52d2329b526003d5dfad43550bae851e60a03718
joshii04 <joshua.sinclair04@gmail.com>
2023-07-02 11:46:37 +0100

    chapter 2
    Finished chapter 2 of the course

--- tsPlaying/course2/build/js/main.js
+++ tsPlaying/course2/build/js/main.js
@@ -1,3 +1,23 @@
 "use strict";
-let username = 'josh';
-console.log(username);
+// - - - - - - - - - - - - - - Types - - - - - - - - - - - - - - //
+let myName = 'dave';
+let myName2;
+let meaningOfLife;
+let isLoading;
+let album; //defeats TS, and becomes like JS
+let album2; //This is a union type; it says string or a number
+// TS keeps the assigned datatype unlike JS
+myName = 'josh';
+meaningOfLife = 5;
+isLoading = true;
+album = true; //use when you don't know the receiveing data type 
+album2 = 'three';
+album2 = 3; // can be both
+// - - - - - - - - Create a fucntion - - - - - - - - - //
+const sum = (a, b) => {
+    return a + b;
+};
+// - - - - - - - - union types - - - - - - - -
+let postId; //good for postcodes
+let isActive; //can be more then 2 data types
+let re = /\w+/g; // this is a reg experssion
--- tsPlaying/course2/src/main.ts
+++ tsPlaying/course2/src/main.ts
@@ -1,2 +1,33 @@
-let username = 'josh'
-console.log(username)
+
+// - - - - - - - - - - - - - - Types - - - - - - - - - - - - - - //
+
+let myName: string = 'dave'
+let myName2: string
+let meaningOfLife: number
+let isLoading: boolean
+let album: any //defeats TS, and becomes like JS
+let album2: string | number  //This is a union type; it says string or a number
+
+
+// TS keeps the assigned datatype unlike JS
+
+myName = 'josh'
+meaningOfLife = 5
+isLoading = true
+album = true //use when you don't know the receiveing data type 
+
+album2 = 'three'
+album2 = 3 // can be both
+
+// - - - - - - - - Create a fucntion - - - - - - - - - //
+
+const sum = (a: number, b: string) =>{
+    return a + b
+}
+
+// - - - - - - - - union types - - - - - - - -
+
+let postId: string | number //good for postcodes
+let isActive: number | boolean //can be more then 2 data types
+
+let re: RegExp = /\w+/g // this is a reg experssion
