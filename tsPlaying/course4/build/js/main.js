1f674b7d735064e4da9acf9e9fa21ed933861810
joshii04 <joshua.sinclair04@gmail.com>
2023-07-19 10:50:47 +0100

    save
    save

--- tsPlaying/course4/build/js/main.js
+++ tsPlaying/course4/build/js/main.js
@@ -1,3 +1,53 @@
 "use strict";
-let username = "josh";
-console.log(username);
+let stringArr = ['one', 'hey', 'josh'];
+let guitars = ['strat', 'les paul', 5150];
+let mixedData = ['EVH', 1984, true];
+stringArr[0] = 'josh';
+stringArr.push('hey');
+guitars[0] = 1984;
+guitars.unshift('jim');
+let test = [];
+let bands = [];
+// ^ Empty array
+bands.push('jola');
+// ^ insert data to the bands string array
+// Tuple 
+let myTuple = ['dave', 42, true];
+// ^ create strict array and their positions
+// objects
+let myObj;
+myObj = [];
+console.log(typeof myObj);
+// show what data type ish
+// example object
+const exampleObj = {
+    prop: 'dave',
+    prop2: true,
+};
+exampleObj.prop = 'josh';
+let evh = {
+    name: 'eddie',
+    active: false,
+    albums: [1984, 5150, 'OU3453']
+};
+let JP = {
+    name: 'Loax',
+    active: true,
+    albums: ['I', 3430, 'IV']
+};
+// - - - - - - - - - -
+const greetGuitarist = (guitarist) => {
+    return `hello ${guitarist.name}!`;
+};
+console.log(greetGuitarist(JP));
+// - - - - - - - - - ENUMS - - - - - - - - - - - -
+var Grade;
+(function (Grade) {
+    Grade[Grade["U"] = 1] = "U";
+    // of an array
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {}));
+console.log(Grade.U);
--- tsPlaying/course4/src/main.ts
+++ tsPlaying/course4/src/main.ts
@@ -1,2 +1,80 @@
-let username = "josh"
-console.log(username)
+let stringArr = ['one', 'hey', 'josh']
+
+let guitars = ['strat', 'les paul', 5150]
+
+let mixedData = ['EVH', 1984, true]
+
+stringArr[0] = 'josh'
+stringArr.push('hey')
+
+guitars[0] = 1984
+guitars.unshift('jim')
+
+let test = []
+let bands: string[] = []
+// ^ Empty array
+bands.push('jola')
+// ^ insert data to the bands string array
+
+// Tuple 
+
+let myTuple: [string, number, boolean] = ['dave', 42, true]
+// ^ create strict array and their positions
+
+// objects
+
+let myObj: object
+myObj =[]
+console.log(typeof myObj)
+// show what data type ish
+
+// example object
+
+const exampleObj = {
+    prop: 'dave',
+    prop2: true,
+}
+
+exampleObj.prop = 'josh'
+
+// - - - - - - - - - -
+
+interface Guitarists {
+    // this is a class
+    name: string,
+    active?: boolean, //makes this type optional
+    albums: (string | number)[]
+}
+
+let evh: Guitarists = {
+    name: 'eddie',
+    active: false,
+    albums: [1984, 5150, 'OU3453']
+}
+
+let JP: Guitarists = {
+    name: 'Loax',
+    active: true,
+    albums: ['I', 3430, 'IV']
+}
+
+// - - - - - - - - - -
+
+const greetGuitarist = (guitarist: Guitarists) => {
+    return `hello ${guitarist.name}!`
+}
+
+console.log(greetGuitarist(JP))
+
+// - - - - - - - - - ENUMS - - - - - - - - - - - -
+
+enum Grade {
+    U = 1, //you can start it all at 1 here rather then like in java when you have to put all of them as specific values
+        // of an array
+    D,
+    C,
+    B,
+    A,
+}
+
+console.log(Grade.U)
